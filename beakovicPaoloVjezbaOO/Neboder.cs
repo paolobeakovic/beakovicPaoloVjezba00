a0ae203ecb947946ede83fdd04de703962fc26e7
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-03-10 11:43:59 +0100

    Add files via upload

---
+++ beakovicPaoloVjezbaOO/Neboder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloVjezbaOO
+{
+    class Neboder
+    {
+        double visina;
+        int brojKatova;
+        string ispis;
+
+        public Neboder(double visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+
+        public double Getvisina()
+        {
+            return visina;
+        }
+
+        public double GetbrojKatova()
+        {
+            return brojKatova;
+        }
+
+        public void setvisina(double visina)
+        {
+            this.visina = visina;
+        }
+
+        public void SetbrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Visina iznosi" + Getvisina() + "\nBroj katova iznosi" + GetbrojKatova());
+            return base.ToString();
+        }
+
+    }
+}
---
+++ beakovicPaoloVjezbaOO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloVjezbaOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neboder tower = new Neboder(200, 10);
+            tower.setvisina(tower.Getvisina() + 10);
+            tower.SetbrojKatova(Convert.ToInt32(tower.GetbrojKatova()+ 1));
+            tower.ToString();
+            Console.ReadKey();
+        }
+    }
+}
